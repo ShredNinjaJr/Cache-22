4fab705e5585a6fd7e2e65b813d11a30377d193f
Vibhakar Vemulapati <vemulpt2@linux-a2.ews.illinois.edu>
2016-03-15 23:45:34 -0500

    Fixed operand typo

--- modules/alu_logic.sv
+++ modules/alu_logic.sv
@@ -14,7 +14,7 @@ begin
 	case(op)
 		op_add: aluop = alu_add;
 		op_not: aluop = alu_not;
-		op_and: aluop = alu_add;
+		op_and: aluop = alu_and;
 		op_shf: begin
 			if(D == 0)
 				aluop = alu_sll;
@@ -27,7 +27,7 @@ begin
 		default: 
 		begin
 			aluop = alu_pass;
-			$display("Unknown aluop");
+		//	$display("Unknown aluop");
 		end
 	endcase
 end
