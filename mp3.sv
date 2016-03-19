bf4943cede55732c6b734b7a6cce3cb5c1e0e2be
unknown <vemulpt2@EVRT-238-35.ad.uillinois.edu>
2016-03-19 18:19:25 -0500

    Mp3

--- mp3.sv
+++ mp3.sv
@@ -2,9 +2,18 @@ import lc3b_types::*;
 
 module mp3
 (
-	input clk
+	input clk,
+	input lc3b_word imem_rdata,
+	input lc3b_word dmem_rdata,
+	input imem_resp,
+	input dmem_resp,
+	
+	output lc3b_word imem_address,
+	output logic imem_read,
+	output logic dmem_read,
+	output logic dmem_write
 );
 
-
+cpu_datapath(.*);
  
  endmodule: mp3
--- mp3_tb.sv
+++ mp3_tb.sv
@@ -2,7 +2,7 @@ module mp3_tb;
 
 timeunit 1ns;
 timeprecision 1ns;
-
+/*
 logic clk;
 logic pmem_resp;
 logic pmem_read;
@@ -10,10 +10,13 @@ logic pmem_write;
 logic [15:0] pmem_address;
 logic [127:0] pmem_rdata;
 logic [127:0] pmem_wdata;
+*/
+logic clk;
 
 /* Clock generator */
 initial clk = 0;
 always #5 clk = ~clk;
+
 /*
 mp3 dut
 (
@@ -37,5 +40,39 @@ physical_memory memory
     .rdata(pmem_rdata)
 );
 */
+logic imem_resp;
+logic imem_read;
+logic [15:0] imem_address;
+logic [15:0] imem_rdata;
+
+logic [15:0] dmem_address;
+logic [15:0] dmem_rdata;
+logic [15:0] dmem_wdata;
+logic dmem_resp;
+logic dmem_read;
+logic dmem_write;
+
+mp3 dut
+(
+	.*
+)
+
+magic_memory_dp
+(
+	.clk,
+	.read_a(imem_read),
+	.write_a(0),
+	.address_a(imem_address),
+	.resp_a(imem_resp),
+	.rdata_a(imem_rdata),
+	
+	.read_b(dmem_read),
+	.write_b(dmem_write),
+	.address_b(dmem_address),
+	.wdata_b(dmem_wdata),
+	.resp_b(dmem_resp),
+	.rdata_b(dmem_rdata)
+	
+);
 
 endmodule : mp3_tb
