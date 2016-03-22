ab3b410e69db2d5a4114649af397b4df743b16dc
unknown <kcperki2@illinois.edu>
2016-03-21 23:44:56 -0500

    updated testbench for issue control. needs actual testing

---
+++ test_benches/cpu_datapath_testbench.sv
@@ -0,0 +1,181 @@
+import lc3b_types::*;
+
+module cpu_datapath_testbench;
+
+timeunit 1ns; 
+timeprecision 1ns;
+
+logic clk;
+
+lc3b_word imem_rdata;
+lc3b_word dmem_rdata;
+logic imem_resp;
+logic dmem_resp;
+
+lc3b_word imem_address;
+logic imem_read;
+logic dmem_read;
+logic dmem_write;
+
+
+cpu_datapath cpu_dp (.*);
+	.
+
+always #1 clk = ~clk;
+
+initial begin: CLOCK_INITIALIZATION
+	clk = 1;
+end
+int ErrCnt = 0;	/* Error count */
+int testnum;
+initial begin: TEST_VECTORS
+#2 flush = 1;
+ErrCnt = 0;
+#4 flush = 0;
+CDB_in = 0;
+
+
+for(int i = 0; i < 10; i++)
+begin
+ test1();
+ test2();
+ test3();
+end
+
+if(ErrCnt == 0)
+begin
+  $display("PASSED TEST CASES!!!");
+end
+else
+begin
+ $display("%d TEST CASES FAILED", ErrCnt);
+end
+
+
+end
+
+
+/* Simple Test, when both operands are given by Issue control */
+task automatic test1();
+#2 flush = 1;
+testnum = 1;
+#2 flush = 0;
+#1 
+/* Simple add test */
+busy_in = 1;
+op_in = op_add;
+Vj = $urandom();
+Vk = $urandom();
+
+#1
+ld_busy = 1;
+issue_ld_Vj = 1;
+issue_ld_Vk = 1;
+
+#2
+
+ld_busy = 0;
+issue_ld_Vj = 0;
+issue_ld_Vk = 0;
+if(CDB_out.data != (Vj+Vk))
+begin
+	ErrCnt++;
+
+	$display("TEST 1 Failed! inputs: %x, %x; Output value %x", Vj, Vk, CDB_out.data);
+$display("Alu add test out = %x; Expected out = %x", CDB_out.data, (Vj+Vk));
+end
+endtask
+
+
+/* Add with waiting on 1 register */
+task test2();
+#2
+flush = 1;
+testnum = 2;
+#2
+flush = 0;
+
+#1
+busy_in = 1;
+op_in = op_add;
+Vj = 16'h32;
+Qk = 3'h3;
+
+#1 
+ld_busy = 1;
+issue_ld_Vj = 1;
+issue_ld_Qk = 1;
+#1
+CDB_in.data = 16'hFFF4;
+CDB_in.valid = 1;
+CDB_in.tag = 3'h3;
+#1
+ld_busy = 0;
+issue_ld_Vj = 0;
+issue_ld_Qk = 0;
+#2
+if(CDB_out.data != (Vj-12))
+begin
+	ErrCnt++;
+	$display("TEST 2 Failed!");
+	$display("Alu add test out = %x; Expected out = %x", CDB_out.data, (Vj - 12));
+	$display(" %x, %x; Output value %x", Vj, Vk, CDB_out.data);
+end
+endtask
+
+task test3();
+
+/* TEST 3 */
+/* AND Test with wrong both registers waiting; CDB outputs wrong tags */
+#2
+flush = 1;
+testnum = 3;
+#2
+flush = 0;
+
+#1
+Qk = 3'h2;
+Qj = 3'h3;
+busy_in = 1;
+op_in = op_and;
+
+#1
+ld_busy = 1;
+issue_ld_Qk = 1;
+issue_ld_Qj = 1;
+
+#2
+ld_busy = 0;
+issue_ld_Qk = 1;
+issue_ld_Qj = 1;
+CDB_in.valid = 0;
+#1
+CDB_in.data = 16'hBAAD;
+CDB_in.valid = 1;
+CDB_in.tag = 3'h4;
+
+#2
+CDB_in.data = 16'h600d;
+CDB_in.valid = 1;
+CDB_in.tag = 3'h3;
+
+#2
+CDB_in.data = 16'hBAAd;
+CDB_in.valid = 1;
+CDB_in.tag = 3'h3;
+
+#2
+CDB_in.data = 16'h600f;
+CDB_in.valid = 1;
+CDB_in.tag = 3'h2;
+
+#3
+if(CDB_out.data != (16'h600d & 16'h600f))
+begin
+	ErrCnt++;
+	$display("TEST 3 Failed!");
+	$display("Inputs %x, %x; Output %x", 16'h600d, 16'h600f, CDB_out.data);
+end
+endtask
+
+endmodule : cpu_datapath_testbench
--- test_benches/issue_control_testbench.sv
+++ test_benches/issue_control_testbench.sv
@@ -9,6 +9,7 @@ timeprecision 1ns;
 	// Fetch -> Issue Control
 	lc3b_word instr;
 	logic instr_is_new;
+	lc3b_word curr_pc;
 	// CDB -> Issue Control
 	CDB CDB_in;
 	// Reservation Station -> Issue Control
@@ -16,31 +17,45 @@ timeprecision 1ns;
 	// ROB -> Issue Control
 	logic rob_full;
 	lc3b_rob_addr rob_addr;
+	lc3b_word rob_sr1_value_out;
+	lc3b_word rob_sr2_value_out;
+	logic rob_sr1_valid_out;
+	logic rob_sr2_valid_out;
 	// Regfile -> Issue Control
-	lc3b_word reg_value [7:0];
-	logic reg_busy [7:0];
-	lc3b_rob_addr reg_robs [7:0];
+	regfile_t sr1_in;
+	regfile_t sr2_in;
+	regfile_t dest_in;
+
+	logic stall;
 	// Issue Control -> Reservation Station
 	lc3b_opcode res_op_in;
 	logic [15:0] res_Vj, res_Vk;
-	logic [2:0] res_Qj, res_Qk, res_dest;
-	logic issue_ld_busy_dest, issue_ld_Vj, issue_ld_Vk, issue_ld_Qk, issue_ld_Qj, issue_ld_validJ, issue_ld_validK;
+	logic [2:0] res_Qj, res_Qk;
+	lc3b_rob_addr res_dest;
+	logic issue_ld_busy_dest, issue_ld_Vj, issue_ld_Vk, issue_ld_Qk, issue_ld_Qj;
 	logic [2:0] res_station_id;
-	logic  res_validJ, res_validK; // [valid J, valid K]
+	// Issue Control -> Load Buffer
+	logic load_buf_write_enable;
+	lc3b_word load_buf_offset;
  	// Issue Control -> ROB
 	logic rob_write_enable;
-	logic [6:0] rob_inst;
+	lc3b_opcode rob_opcode;
+	lc3b_reg rob_dest;
 	logic [15:0] rob_value_in;
 	// Issue Control -> Regfile
-	lc3b_reg reg_dest;
+	lc3b_reg reg_dest, sr1, sr2;
 	logic ld_reg_busy_dest;
 	lc3b_rob_addr reg_rob_entry;
+	logic [3] rob_sr1_read_addr;
+	logic [3[ rob_sr2_read_addr;
+	
 
 
 issue_control I_C(
 	.clk(clk),
 	.instr(instr),
 	.instr_is_new(instr_is_new),
+	.curr_pc(curr_pc),
 	.CDB_in(CDB_in),
 	.alu_res1_busy(alu_res1_busy),
 	.alu_res2_busy(alu_res2_busy),
@@ -49,31 +64,37 @@ issue_control I_C(
 	.st_buffer_full(st_buffer_full),
 	.rob_full(rob_full),
 	.rob_addr(rob_addr),
-	.reg_value(reg_value),
-	.reg_busy(reg_busy),
-	.reg_robs(reg_robs),
+	.rob_sr1_value_out(rob_sr1_value_out);
+	.rob_sr2_value_out(rob_sr2_value_out);
+	.sr1_in(sr1_in);
+	.sr2_in(sr2_in);
+	.dest_in(dest_in);
+	.stall(stall);
 	.res_op_in(res_op_in),
 	.res_Vj(res_Vj),
 	.res_Vk(res_Vk),
 	.res_Qj(res_Qj),
 	.res_Qk(res_Qk),
 	.res_dest(res_dest),
-	.res_validJ(res_validJ),
-	.res_validK(res_validK),
 	.issue_ld_busy_dest(issue_ld_busy_dest),
 	.issue_ld_Vj(issue_ld_Vj),
 	.issue_ld_Vk(issue_ld_Vk),
 	.issue_ld_Qj(issue_ld_Qj),
 	.issue_ld_Qk(issue_ld_Qk),
-	.issue_ld_validJ(issue_ld_validJ),
-	.issue_ld_validK(issue_ld_validK),
 	.res_station_id(res_station_id),
+	.load_buf_write_enable(load_buf_write_enable),
+	.load_buf_offset(load_buf_offset),
 	.rob_write_enable(rob_write_enable),
-	.rob_inst(rob_inst),
+	.rob_opcode(rob_opcode),
+	.reg_dest(reg_dest),
 	.rob_value_in(rob_value_in),
 	.reg_dest(reg_dest),
+	.sr1(sr1),
+	.sr2(sr2),
 	.ld_reg_busy_dest(ld_reg_busy_dest),
-	.reg_rob_entry(reg_rob_entry)
+	.reg_rob_entry(reg_rob_entry),
+	.rob_sr1_read_addr(rob_sr1_read_addr),
+	.rob_sr2_read_addr(rob_sr2_read_addr)
 );
 
 always #1 clk = ~clk;
@@ -99,44 +120,6 @@ st_buffer_full = 0;
 
 rob_full = 0;
 rob_addr = 1;
-
-reg_value[0] = 0;
-reg_value[1] = 4;
-reg_value[2] = 19;
-reg_busy[0] = 0;
-reg_busy[1] = 0;
-reg_busy[2] = 0;
-reg_robs[0] = 0;
-
-#2
-instr = 16'b0101100000000001; // R4 <- R0 & R1
-instr_is_new = 1;
-
-CDB_in.valid = 1;
-CDB_in.tag = 1;
-CDB_in.data = 2523;
-
-alu_res1_busy = 1;
-alu_res2_busy = 1;
-alu_res3_busy = 0;
-
-rob_addr = 2;
-
-reg_busy[0] = 1;
-reg_busy[1] = 1;
-reg_robs[1] = 1;
-
-#2
-instr_is_new = 1;
-alu_res1_busy = 1;
-alu_res2_busy = 1;
-alu_res3_busy = 1;
-
-
-
-
-
-
 
 
 end
